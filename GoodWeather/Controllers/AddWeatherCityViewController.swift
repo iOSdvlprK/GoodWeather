d2c12e93f140db4d8eb98dc347b13957ff52dc51
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-03-22 00:00:55 +0900

    implement main user interface

---
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by joe on 3/21/24.
+//
+
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by joe on 3/21/24.
+//
+
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
