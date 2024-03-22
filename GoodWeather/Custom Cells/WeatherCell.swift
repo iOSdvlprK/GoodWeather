b3a1a9cc4132f95d0f88cb8b1e935e7a36281b99
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-03-23 01:44:45 +0900

    display hard-coded weather into table view

---
+++ GoodWeather/Custom Cells/WeatherCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by joe on 3/22/24.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
