c0036082766141322beeb29297ca513b25d5bf66
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-03-26 23:52:26 +0900

    pass values to parent screen using delegate

--- GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -7,18 +7,28 @@
 
 import UIKit
 
+protocol AddWeatherDelegate {
+    func addWeatherDidSave(vm: WeatherViewModel)
+}
+
 class AddWeatherCityViewController: UIViewController {
     @IBOutlet weak var cityNameTextField: UITextField!
+    
+    var delegate: AddWeatherDelegate?
     
     @IBAction func saveCityButtonPressed() {
         if let city = cityNameTextField.text {
             let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric")!
-            let weatherResource = Resource<Any>(url: weatherURL) { data in
-                return data
+            let weatherResource = Resource<WeatherViewModel>(url: weatherURL) { data in
+                let weatherVM = try? JSONDecoder().decode(WeatherViewModel.self, from: data)
+                return weatherVM
             }
             
-            Webservice().load(resource: weatherResource) { result in
-                
+            Webservice().load(resource: weatherResource) { [weak self] result in
+                if let weatherVM = result, let delegate = self?.delegate {
+                    delegate.addWeatherDidSave(vm: weatherVM)
+                    self?.dismiss(animated: true)
+                }
             }
         }
     }
---
+++ GoodWeather/View Models/WeatherViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherViewModel.swift
+//  GoodWeather
+//
+//  Created by joe on 3/24/24.
+//
+
+import Foundation
+
+struct WeatherListViewModel {
+    private var weatherViewModels = [WeatherViewModel]()
+}
+
+struct WeatherViewModel: Decodable {
+    let name: String
+    let currentTemperature: TemperatureViewModel
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case currentTemperature = "main"
+    }
+}
+
+struct TemperatureViewModel: Decodable {
+    let temperature: Double
+    let temperatureMin: Double
+    let temperatureMax: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case temperature = "temp"
+        case temperatureMin = "temp_min"
+        case temperatureMax = "temp_max"
+    }
+}
