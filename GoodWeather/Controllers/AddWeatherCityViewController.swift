dc3e60cfdd6fbdcda64e42465ccd91a591a8f37f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-04-02 01:20:12 +0900

    setup view to view model binding

--- GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -12,13 +12,29 @@ protocol AddWeatherDelegate {
 }
 
 class AddWeatherCityViewController: UIViewController {
-    @IBOutlet weak var cityNameTextField: UITextField!
-    @IBOutlet weak var stateTextField: UITextField!
-    @IBOutlet weak var zipCodeTextField: UITextField!
+    private var addCityViewModel = AddCityViewModel()
+    
+    @IBOutlet weak var cityNameTextField: BindingTextField! {
+        didSet {
+            cityNameTextField.bind { self.addCityViewModel.city = $0 }
+        }
+    }
+    @IBOutlet weak var stateTextField: BindingTextField! {
+        didSet {
+            stateTextField.bind { self.addCityViewModel.state = $0 }
+        }
+    }
+    @IBOutlet weak var zipCodeTextField: BindingTextField! {
+        didSet {
+            zipCodeTextField.bind { self.addCityViewModel.zipCode = $0 }
+        }
+    }
     
     var delegate: AddWeatherDelegate?
     
     @IBAction func saveCityButtonPressed() {
+        print(self.addCityViewModel)
+        
         if let city = cityNameTextField.text {
             let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric")!
             let weatherResource = Resource<WeatherViewModel>(url: weatherURL) { data in
